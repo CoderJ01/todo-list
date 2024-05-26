c547f6d93528a818f0aa066c559fd14f587ae180
CoderJ01 <buildingguy@sbcglobal.net>
2024-05-25 19:44:55 -0700

    clean

--- config.php
+++ config.php
@@ -6,6 +6,6 @@
     $conn = new mysqli($server, $username, $password, $database_name);
 
     if($conn->connect_error) {
-        die("Connection Failed" . $conn->connect_error);
+        die('Connection Failed' . $conn->connect_error);
     }
 ?>
