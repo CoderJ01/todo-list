3dca2557f22895e154a5d6eca5cf537d1648dfea
CoderJ01 <buildingguy@sbcglobal.net>
2024-05-30 13:53:13 -0700

    change

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     $db_username = 'root';
     $db_password = '';
     $db_name = 'todo'; 
-    // $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
+    $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
 
     $db = mysqli_connect($db_server, $db_username, $db_password, $db_name);
 
