8748a45f128863ce487221949303cd5461520c27
CoderJ01 <buildingguy@sbcglobal.net>
2024-05-30 13:32:33 -0700

    adjust SQL db connection configuration

--- config.php
+++ config.php
@@ -3,9 +3,11 @@
     $db_username = 'root';
     $db_password = '';
     $db_name = 'todo'; 
-    $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
+    // $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
 
-    if($conn->connect_error) {
-        die('Connection Failed' . $conn->connect_error);
+    $db = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+
+    if(mysqli_connect_errno()) {
+        die('Connection Failed: ' . mysqli_connect_error());
     }
 ?>
