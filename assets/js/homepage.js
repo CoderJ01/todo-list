71dffa0c21a86fbe0a4c0475febc382bc9a6c287
CoderJ01 <buildingguy@sbcglobal.net>
2024-05-29 18:51:20 -0700

    enable todo-cookie to be removed from browser

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,7 @@
 let modal = document.getElementById('myModal');
 let btn = document.getElementById('myBtn');
 let span = document.getElementsByClassName('close')[0];
+let logout = document.getElementsByClassName('logout')[0];
 
 btn.onclick = function() {
     modal.style.display = 'block';
@@ -14,4 +15,15 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
     }
+}
+
+function getCookie(name) {
+    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+}
+
+logout.onclick = function() {
+    document.cookie = "todo-cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.reload(false);
 }
