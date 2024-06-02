20b3223703ec447f30e477b5846cc65085156eed
CoderJ01 <buildingguy@sbcglobal.net>
2024-06-01 19:59:16 -0700

    clean

--- config.php
+++ config.php
@@ -3,7 +3,6 @@
     $db_username = 'root';
     $db_password = '';
     $db_name = 'todo'; 
-    // $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
 
     $db = new mysqli ($db_server, $db_username, $db_password, $db_name);
 
