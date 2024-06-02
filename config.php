e461403391124583e7c5bd753c7632367c337dc6
CoderJ01 <buildingguy@sbcglobal.net>
2024-06-01 21:28:58 -0700

    clean

--- config.php
+++ config.php
@@ -7,7 +7,6 @@
     try {
         $db = new PDO("mysql:host=$db_server;dbname=$db_name", $db_username, $db_password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
     } 
     catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
