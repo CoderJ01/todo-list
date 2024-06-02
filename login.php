c2712f9b8e12888da3f46b2ee1a63511d8fa0acf
CoderJ01 <buildingguy@sbcglobal.net>
2024-06-01 21:07:08 -0700

    use prepared statements to protect against SQL injection attack

--- login.php
+++ login.php
@@ -43,54 +43,58 @@
         $email = strip_tags($_POST['email']); // strip_tags() protect against XSS attacks
         $password = strip_tags($_POST['password']);
 
-        // protect from SQL injection attack
-        $protected_email = mysqli_real_escape_string($db, $email);
-        $protected_password = mysqli_real_escape_string($db, $password);
+        // // protect from SQL injection attack
+        // $protected_email = mysqli_real_escape_string($db, $email);
+        // $protected_password = mysqli_real_escape_string($db, $password);
 
         // get all users' emails from todo.users table
-        $sql_get_emails = "SELECT email FROM `users`";
-        $result = mysqli_query($db, $sql_get_emails);
-  
-        if ($result->num_rows > 0) {
+        $stmt = $db->prepare("SELECT email FROM `users`"); // prepare statement protects against SQL injection attack
+        $stmt->execute();
+        $emails = $stmt->fetchAll();
+
+        if(count($emails) > 0) {
             $match = false;
-            while($row = $result->fetch_assoc()) {
-                if($row["email"] === $email) {
+
+            foreach($emails as $email) {
+                if($email["email"] === $email) {
                     $match = true; // email exists in the database
                 }
             }
+
             if($match == true) {
                 $correct_password = false;
-
+    
                 // get password associated with email
-                $sql_get_user_password = "SELECT user_password FROM `users` WHERE email='$protected_email'";
-                $result = mysqli_query($db, $sql_get_user_password);
-
-                while($row = $result->fetch_assoc()) {
+                $stmt = $db->prepare("SELECT user_password FROM `users` WHERE email='$email'");
+                $stmt->execute();
+                $passwords = $stmt->fetchAll();
+    
+                foreach($passwords as $password) {
                     // verify that user input the correct password
-                    if(password_verify($protected_password, $row["user_password"])) {
+                    if(password_verify($password, $password["user_password"])) {
                         $correct_password = true; // password matches w/ email
                     }
                 }
                 if($correct_password === true) {
                     echo "<br/><p style='color: white; text-align: center'>You have successfully logged in!</p>";
-
+    
                     // cookie data
                     $cookie_name = 'todo-cookie';
                     $cookie_value =  substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 1, 80); // random string
                     $hashed_cookie = password_hash($cookie_value, PASSWORD_BCRYPT, [ "cost" => 15 ]);
-
+    
                     // set cookie
                     setcookie($cookie_name, $hashed_cookie, time() + (86400 * 30), '/');
-
-                    $sql = "UPDATE `users` SET user_cookie = '$hashed_cookie' WHERE email='$protected_email'";
-                    $result = mysqli_query($db, $sql);
+    
+                    $stmt = $db->prepare("UPDATE `users` SET user_cookie = '$hashed_cookie' WHERE email='$protected_email'");
+                    $stmt->execute();
                     // $conn->close();
                     header("Location: http://localhost:3000/homepage.php");
                 }
                 else {
                     echo "<br/><p style='color: white; text-align: center'>The password does not match the email!</p>";
                 }
-            }
+            } 
             else {
                 echo "<br/><p style='color: white; text-align: center'>This email has yet to be registered!</p>";
             }
