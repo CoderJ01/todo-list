a00062b6febddde685271c1305b543a3b750675a
CoderJ01 <buildingguy@sbcglobal.net>
2024-06-01 20:41:11 -0700

    change database configuration

--- config.php
+++ config.php
@@ -4,9 +4,12 @@
     $db_password = '';
     $db_name = 'todo'; 
 
-    $db = new mysqli ($db_server, $db_username, $db_password, $db_name);
-
-    if(mysqli_connect_errno()) {
-        die('Connection Failed: ' . mysqli_connect_error());
+    try {
+        $db = new PDO("mysql:host=$db_server;dbname=$db_name", $db_username, $db_password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } 
+    catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
 ?>
