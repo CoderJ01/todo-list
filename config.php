72507545ab80b3cba6d1db429308220bdd494479
CoderJ01 <buildingguy@sbcglobal.net>
2024-06-01 20:43:20 -0700

    change

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     $db_server = 'localhost';
+    $db_name = 'todo';
     $db_username = 'root';
-    $db_password = '';
-    $db_name = 'todo'; 
+    $db_password = ''; 
 
     try {
         $db = new PDO("mysql:host=$db_server;dbname=$db_name", $db_username, $db_password);
