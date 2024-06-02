40ae0490be8c7fbffa1de818b4853cc2fc744f6e
CoderJ01 <buildingguy@sbcglobal.net>
2024-06-01 18:02:43 -0700

    adjust configuration

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     $db_name = 'todo'; 
     // $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
 
-    $db = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+    $db = new mysqli ($db_server, $db_username, $db_password, $db_name);
 
     if(mysqli_connect_errno()) {
         die('Connection Failed: ' . mysqli_connect_error());
