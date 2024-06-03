99120173ad528daf5be0b25a4e79112e08bfb4b8
CoderJ01 <buildingguy@sbcglobal.net>
2024-06-02 22:02:11 -0700

    clean

--- login.php
+++ login.php
@@ -72,17 +72,6 @@
                 }
                 if($correct_password === true) {
                     echo "<br/><p style='color: white; text-align: center'>You have successfully logged in!</p>";
-
-                    // // cookie data
-                    // $cookie_name = 'todo-cookie';
-                    // $cookie_value =  substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 1, 80); // random string
-                    // $hashed_cookie = password_hash($cookie_value, PASSWORD_BCRYPT, [ "cost" => 15 ]);
-
-                    // // set cookie
-                    // setcookie($cookie_name, $hashed_cookie, time() + (86400 * 30), '/');
-
-                    // $sql = "UPDATE `users` SET user_cookie = '$hashed_cookie' WHERE email='$protected_email'";
-                    // $result = mysqli_query($db, $sql);
                     echo "<script>window.location='" . $domain . "/cookie.php'</script>";
                 }
                 else {
